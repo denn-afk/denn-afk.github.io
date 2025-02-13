c20801c96546ccda765a81baffc52d4431c65e93
denn-afk <59733574+denn-afk@users.noreply.github.com>
2025-02-13 17:41:12 -0500

    Update script.js

--- projects/script.js
+++ projects/script.js
@@ -8,20 +8,20 @@ document.getElementById('searchInput').addEventListener('input', function () {
     });
   });
   
-  // 排序功能
-  document.getElementById('sortSelect').addEventListener('change', function () {
-    const sortBy = this.value;
-    const projectList = document.getElementById('projectList');
-    const projects = Array.from(projectList.children);
-  
-    projects.sort((a, b) => {
-      if (sortBy === 'name') {
-        return a.dataset.name.localeCompare(b.dataset.name);
-      } else if (sortBy === 'date') {
-        return new Date(a.dataset.date) - new Date(b.dataset.date);
-      }
-    });
-  
-    projects.forEach(project => projectList.appendChild(project));
-  });
+// 排序功能
+document.getElementById('sortSelect').addEventListener('change', function () {
+const sortBy = this.value;
+const projectList = document.getElementById('projectList');
+const projects = Array.from(projectList.children);
+
+projects.sort((a, b) => {
+    if (sortBy === 'name') {
+    return a.dataset.name.localeCompare(b.dataset.name);
+    } else if (sortBy === 'date') {
+    return new Date(a.dataset.date) - new Date(b.dataset.date);
+    }
+});
+
+projects.forEach(project => projectList.appendChild(project));
+});
   
